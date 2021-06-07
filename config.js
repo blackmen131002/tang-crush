b7b2e8d6ada8edef3e90191a711e9728d084d8e9
blackmen131002 <85465256+blackmen131002@users.noreply.github.com>
2021-06-07 08:22:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Khong co thich :3',
-    question:'Nhớ Linh chẳng biết nhắn gì, Nhắn yêu thì ngại tại vì ít văn, Trong lòng thì cứ băn khoăn, Thôi thì nhắn đại nay Linh ăn gì?  <3',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Oi mừng quá 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay mình cùng trò chuyện tiếp nhé Công chúa <3.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'instagram.com/ptiendat15'  
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
